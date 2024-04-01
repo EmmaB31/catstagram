e283f309a66731a4b6984da792f53125d164cf21
Emma Bosworth <emmab3105@gmail.com>
2024-04-01 19:03:29 +0100

    set up login page

---
+++ src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="login">
+        <form @submit.prevent="handleLogin">
+            <label>Username:</label>
+            <input type="text"  id="username" v-model="username">
+            <br>
+            <label>Password:</label>
+            <input type="password" id="password" v-model="password">
+            <br>
+            <button>Login</button>
+        </form>
+            <p> This is the login Page</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                username: '',
+                password: ''
+            }
+        },
+        methods: {
+            handleLogin() {
+                if(this.username !== '' && this.password !== '' ) {
+                    this.$router.push('/')
+                }
+                else {
+                    alert('Please enter username and password')
+                }
+                console.log('login')
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+
+</style>
